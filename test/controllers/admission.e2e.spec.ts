c5022230eb338b187f84492dc13464a1ac648d16
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-06-30 17:53:20 -0500

    revert rc2 usage of kubernetes client library

--- test/controllers/admission.e2e.spec.ts
+++ test/controllers/admission.e2e.spec.ts
@@ -8,21 +8,17 @@ describe('controllers/admission', () => {
   let client: CoreV1Api
 
   async function deletePods () : Promise<unknown> {
-    const pods = await client.listNamespacedPod({
-      namespace: TEST_NAMESPACE
-    })
-    const deleteProms = pods.items.map((pod: V1Pod) => client.deleteNamespacedPod(pod.metadata as any))
+    const pods = await client.listNamespacedPod(TEST_NAMESPACE)
+    const deleteProms = pods.body.items.map((pod: V1Pod) => client.deleteNamespacedPod(pod.metadata?.name as string, pod.metadata?.namespace as string))
     return Promise.all(deleteProms)
   }
   async function deleteNamespace () : Promise<void> {
     try {
-      if ((await client.listNamespace()).items.some((ele:V1Namespace) => {
+      if ((await client.listNamespace()).body.items.some((ele:V1Namespace) => {
         return ele.metadata?.name === TEST_NAMESPACE
       })) {
         await deletePods()
-        await client.deleteNamespace({
-          name: TEST_NAMESPACE
-        })
+        await client.deleteNamespace(TEST_NAMESPACE, undefined, undefined, 0, true)
         // wait for namespace to terminate, this could be more elegant and watch for termination to complete
         await new Promise((resolve) => { setTimeout(resolve, 5000) })
       }
@@ -40,12 +36,10 @@ describe('controllers/admission', () => {
     try {
       await deleteNamespace()
       await client.createNamespace({
-        body: {
-          metadata: {
-            name: TEST_NAMESPACE
-          }
+        metadata: {
+          name: TEST_NAMESPACE
         }
-      })
+      } as V1Namespace)
     } catch (err) {
       console.error(err)
     }
@@ -54,24 +48,21 @@ describe('controllers/admission', () => {
     await deleteNamespace()
   })
   it('Should enhance busybox', async () => {
-    const resp = await client.createNamespacedPod({
-      namespace: TEST_NAMESPACE,
-      body: {
-        metadata: {
-          name: `test-enhance-${randomUUID()}`,
-          namespace: TEST_NAMESPACE
-        },
-        spec: {
-          containers: [{
-            image: 'busybox',
-            name: 'busybox'
-          }]
-        }
+    const resp = await client.createNamespacedPod(TEST_NAMESPACE, {
+      metadata: {
+        name: `test-enhance-${randomUUID()}`,
+        namespace: TEST_NAMESPACE
+      },
+      spec: {
+        containers: [{
+          image: 'busybox',
+          name: 'busybox'
+        }]
       }
-    })
-    expect(resp.status?.message).toEqual('Created')
-    expect(resp.spec?.securityContext?.runAsNonRoot).toBeTruthy()
-    resp.spec?.containers.forEach((c) => {
+    } as V1Pod)
+    expect(resp.response.statusMessage).toEqual('Created')
+    expect(resp.body.spec?.securityContext?.runAsNonRoot).toBeTruthy()
+    resp.body.spec?.containers.forEach((c) => {
       expect(c.securityContext?.allowPrivilegeEscalation).toBeFalsy()
       expect(c.securityContext?.privileged).toBeFalsy()
       expect(c.securityContext?.readOnlyRootFilesystem).toBeTruthy()
