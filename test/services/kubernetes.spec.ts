c8ab01b5723431b8816fbc1b7b96fcf91097a37f
bryopsida <8363252+bryopsida@users.noreply.github.com>
2023-07-01 16:20:20 -0500

    add test stubs

---
+++ test/services/kubernetes.spec.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from '@jest/globals'
+import 'reflect-metadata'
+
+describe('services/kubernetes', () => {
+  describe('syncCaBundle', () => {
+    it('should update web hook configuration when caBundle does not match', () => {
+      expect(false).toBe(true)
+    })
+    it('should not update web hook configuration when caBundle matches', () => {
+      expect(false).toBe(true)
+    })
+    it('should resolve false on error', () => {
+      expect(false).toBe(true)
+    })
+  })
+})
